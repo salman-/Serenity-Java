95d43045ca88748099e2d5db6a2ef4516b4e5826
salmanlashkarara <salmanlashkarara@gmail.com>
2025-06-14 19:31:23 +0200

    Add bussiness action for login

---
+++ src/main/java/starter/actions/LoginActions.java
@@ -0,0 +1,19 @@
+package starter.actions;
+
+import net.serenitybdd.annotations.Step;
+import net.serenitybdd.core.steps.UIInteractionSteps;
+import starter.pages.LoginPage;
+import starter.utility.UserType;
+
+public class LoginActions extends UIInteractionSteps {
+
+    LoginPage loginPage;
+
+    @Step("Login as {0}")
+    public void login(String url, UserType user) {
+        openUrl(url);
+        loginPage.insertUsername(user.getUsername());
+        loginPage.insertPassword(user.getPassword());
+        loginPage.clickLoginButton();
+    }
+}

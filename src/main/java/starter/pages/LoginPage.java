3aa7f4d46eee85f3434bcefa861dfb2665b259d4
salmanlashkarara <salmanlashkarara@gmail.com>
2025-06-14 19:31:03 +0200

    Add login page

---
+++ src/main/java/starter/pages/LoginPage.java
@@ -0,0 +1,40 @@
+package starter.pages;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
+
+public class LoginPage extends PageObject {
+
+    @FindBy(css = ".title")
+    private WebElementFacade pageTitle;
+
+    @FindBy(id = "user-name")
+    private  WebElementFacade userNameInput;
+
+    @FindBy(id = "password")
+    private WebElementFacade passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElementFacade loginButton;
+
+    public String getTitle() {
+        return pageTitle.getText();
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+    public void insertPassword(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void insertUsername(String username) {
+        userNameInput.sendKeys(username);
+    }
+
+
+
+}
